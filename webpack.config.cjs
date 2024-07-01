9b11106f0784a61bb33fc9ddf9f103053fbe537b
isellar <ian@sellarfamily.com>
2024-07-01 20:06:38 +0000

    clean up eslint

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,58 +2,57 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: './src/index.tsx',
-    devServer: {
-        host: 'localhost',
-        port: 8085,
-        hot: true,
-        static: {directory: __dirname },
-        allowedHosts: 'all',
-        client: {
-            overlay: false,
-        },
-        devMiddleware: {
-            writeToDisk: true,
-        },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  devServer: {
+    host: 'localhost',
+    port: 8085,
+    static: {directory: __dirname},
+    allowedHosts: 'all',
+    client: {
+      overlay: false,
     },
-    output: {
-        filename: './[name].min.js',
+    devMiddleware: {
+      writeToDisk: true,
     },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                ['@babel/preset-env', { targets: 'defaults' }],
-                                [
-                                    '@babel/preset-react',
-                                    {
-                                        runtime: 'automatic'
-                                    },
-                                ],
-                                '@babel/preset-typescript',
-                            ],
-                            cacheDirectory: false,
-                            cacheCompression: false,
-                        },
-                    },
+  },
+  output: {
+    filename: './[name].min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {targets: 'defaults'}],
+                [
+                  '@babel/preset-react',
+                  {
+                    runtime: 'automatic',
+                  },
                 ],
+                '@babel/preset-typescript',
+              ],
+              cacheDirectory: false,
+              cacheCompression: false,
             },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
 };
